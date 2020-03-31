8433d5c5f32f6eccd3522f0816ff715dbe43415d
Rahul Atlury <atlury@users.noreply.github.com>
2020-03-31 23:29:58 +0530

    Update 001preprepare.sh

--- 001preprepare.sh
+++ 001preprepare.sh
@@ -5,8 +5,8 @@ echo "Launching apk update...."
 apk update
 
 echo "Creating virtual package for musl, musl-dev, musl-utils ..."
-apk add -t musl
-apk add -t musl-dev
-apk add -t musl-utils
+#apk add -t musl
+#apk add -t musl-dev
+#apk add -t musl-utils
 
-echo "Touch musl missing library files!"
+#echo "Touch musl missing library files!"
