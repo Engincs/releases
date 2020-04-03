9fc3d92445e406c8129ed88b435eef2b44b912a5
Rahul Atlury <atlury@users.noreply.github.com>
2020-04-04 00:30:31 +0530

    Update 001staticbuild-apk-abuild.sh

--- 001staticbuild-apk-abuild.sh
+++ 001staticbuild-apk-abuild.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 set -e
 # Runme inside previously built chroot
+apk update
+
+# vi /etc/ssh/sshd_config
+# rc-status
+# rc-service sshd restart
+
+apk add alpine-sdk
+apk add build-base
+apk add bash
+apk add curl
+apk add zlib
+apk add vim
+apk add openssl-dev
+apk add zlib-dev
+apk add musl-dev
+apk add zlib-static
+apk add openssl-libs-static
+
+# vi /etc/passwd change from root:x:0:0:root:/root:/bin/ash to root:x:0:0:root:/root:/bin/bash
+
+# Clone aports and fetch the latest updates
+cd /root/
+git clone https://gitlab.alpinelinux.org/alpine/aports.git
+git pull 
+
+# Static compile abuild
+# Note: If we pass -no-pie to GCC on Alpine, its binary will not be PIE-enabled just like Ubuntu binary isn't PIE-enabled, 
+# but then it will be statically linked as we want
+# Do not attemp on glibc as libc uses libnss to support a number of different providers for address resolution services.
+# Unfortunately, we cannot statically link libnss, as exactly what providers it loads depends on the local system's configuration
+# Hence below command will fail in glibc systems even with enable --enable-static-nss
+cd /root/aports/main/abuild
+CFLAGS="-no-pie -static" make
+
