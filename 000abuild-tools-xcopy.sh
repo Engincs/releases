67d50ae7b77d40c2c61484c6a941b67164f82ea6
Rahul Atlury <atlury@users.noreply.github.com>
2020-04-07 18:46:17 +0530

    Create 000abuild-tools-xcopy.sh

---
+++ 000abuild-tools-xcopy.sh
@@ -0,0 +1,13 @@
+echo "Copy tools to common storage"
+#1. patch - copy - patch
+#2. pax-utils - copy - scanelf
+#4. tar - (libattr, libacl, tar) - static compile in host
+#6. attr (on host)
+
+echo "Build busybox"
+
+# echo "Chroot and run apk update"
+# chroot $CHROOT/ /bin/sh -l
+# apk update
+# exit
+# echo "Exited chroot"
