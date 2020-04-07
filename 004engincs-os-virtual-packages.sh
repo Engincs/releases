21808183a81d45bb72811dd39db652f991a8e112
Rahul Atlury <atlury@users.noreply.github.com>
2020-04-08 02:04:20 +0530

    Update 004engincs-os-virtual-packages.sh

--- 004engincs-os-virtual-packages.sh
+++ 004engincs-os-virtual-packages.sh
@@ -71,6 +71,10 @@ cp /lib/apk/db/installed /lib/apk/db/installed-backup
 # printf 'p:so:libc.so.6=6\nF:lib/x86_64-linux-gnu\nR:libc.so.6' >> /lib/apk/db/installed
 printf 'p:so:libc.so.6=6 so:libcrypt.so.1=1 so:libdl.so.2=2 so:libm.so.6=6 so:libpthread.so.0=0 so:libutil.so.1=1 so:librt.so.1=1\nF:lib/x86_64-linux-gnu\nR:libc.so.6\nR:libcrypt.so.1\nR:libdl.so.2\nR:libm.so.6\nR:libpthread.so.0\nR:libutil.so.1\nR:librt.so.1' >> /lib/apk/db/installed
 
-echo "Please make sure to run abuild-keygen with a and i options"
+echo "Please make sure to run abuild-keygen with a and i options\n"
 # abuild-keygen -a -i 
 # which created keys and wrote in the host /etc/abuild.conf file automatically
+echo "============================================================\n"
+echo "Edit /usr/share/abuild/functions.sh to replace musl with gnu"
+echo "============================================================\n"
+echo "Create links in /bin folder for gcc"
