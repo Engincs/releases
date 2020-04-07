619044143c4cce0144d594b28aac0a315f4aaa3f
Rahul Atlury <atlury@users.noreply.github.com>
2020-04-07 18:00:32 +0530

    Update 000abuild-tools.sh

--- 000abuild-tools.sh
+++ 000abuild-tools.sh
@@ -46,7 +46,7 @@ cd /root/
 
 apt -y install build-essential autoconf bison flex texinfo help2man gawk libtool libncurses5-dev python3-dev python3-distutils git gettext libtool-bin libtool-doc 
 apt -y install ncurses-dev bison texinfo flex autoconf automake libtool patch curl cvs build-essential subversion gawk gperf libncurses5-dev libexpat1-dev
-apt-get install zliblg-dev
+apt-get install zlib1g-dev
 apt-get install libssl-dev
 
 echo "Getting apktools static version 64-bit"
