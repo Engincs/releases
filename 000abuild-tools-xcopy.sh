ab1039d446b5bbac6447d9a64f463446474556c2
Rahul Atlury <atlury@users.noreply.github.com>
2020-04-08 23:50:39 +0530

    Update 000abuild-tools-xcopy.sh

--- 000abuild-tools-xcopy.sh
+++ 000abuild-tools-xcopy.sh
@@ -8,22 +8,22 @@ cp /usr/bin/patch /root/storage
 cp /usr/bin/scanelf /root/storage
 cp /usr/bin/make /root/storage
 
-echo "Compiling static version of attr to common storage"
-cd /root/storage
-wget http://download.savannah.gnu.org/releases/attr/attr-2.4.48.tar.gz
-tar xvzf attr-2.4.48.tar.gz
-cd /root/storage/attr-2.4.48
-./configure --prefix=/usr --enable-static --disable-shared
-make LDFLAGS=-static
+#echo "Compiling static version of attr to common storage"
+#cd /root/storage
+#wget http://download.savannah.gnu.org/releases/attr/attr-2.4.48.tar.gz
+#tar xvzf attr-2.4.48.tar.gz
+#cd /root/storage/attr-2.4.48
+#./configure --prefix=/usr --enable-static --disable-shared
+#make LDFLAGS=-static
 
-echo "Compiling static version of tar to common storage"
-apt -y install libacl1-dev libattr1-dev
-cd /root/storage
-wget https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz
-tar xvzf tar-1.32.tar.gz
-cd /root/storage/tar-1.32
-./configure --prefix=/usr --disable-nls --libexecdir=/usr/bin
-make LDFLAGS=-static
+#echo "Compiling static version of tar to common storage"
+#apt -y install libacl1-dev libattr1-dev
+#cd /root/storage
+#wget https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz
+#tar xvzf tar-1.32.tar.gz
+#cd /root/storage/tar-1.32
+#./configure --prefix=/usr --disable-nls --libexecdir=/usr/bin
+#make LDFLAGS=-static
 
 cd /root/storage
 #1. patch - copy - patch
